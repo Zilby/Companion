8255c2252adc2a4ffa500040f6365f0dc437e9d6
Alex Zilbersher <azilby@gmail.com>
2025-03-15 17:02:52 -0400

    head rotations

--- Assets/Scripts/LumpyController.cs
+++ Assets/Scripts/LumpyController.cs
@@ -20,12 +20,12 @@ public class LumpyController : MonoBehaviour
 		Tired, 
 		Asleep, 
 		Inquisitive,
+		Pleased,
 	}
 
-	private Color DEFAULT_COLOR = new Color(5f / 255f, 191 / 255f, 183 / 255f);  // 05BFB7 new Color(0, 134 / 255f, 191 / 255f); 
-	private const float INTENSITY = 1f; // 2.1f; //1.188807F;
-
 	public static LumpyController instance;
+
+	public GameObject face;
 
 	public SkinnedMeshRenderer eyes;
 
@@ -33,9 +33,7 @@ public class LumpyController : MonoBehaviour
 
 	public List<AudioClip> dialogueAudioClips;
 
-	[System.NonSerialized]
-
-	private Expression currentExpression = Expression.Neutral;
+	private Expression currentExpression = Expression.Asleep;
 
 	private Coroutine talk;
 
@@ -49,7 +47,12 @@ public class LumpyController : MonoBehaviour
 		aSource = GetComponent<AudioSource>();
 	}
 
-	public void PlaySegment(int index) 
+    private void Start()
+    {
+		StartCoroutine(SetExpression(Expression.Asleep));
+    }
+
+    public void PlaySegment(int index) 
 	{
 		aSource.clip = dialogueAudioClips[index];
 		aSource.Play();
@@ -62,7 +65,7 @@ public class LumpyController : MonoBehaviour
 			yield break;
 		}
 		this.currentExpression = expression;
-		float duration = 0.1f;
+		float duration = 0.15f;
 		float timeElapsed = 0;
 		Dictionary<int, float> oldEyeWeights = new Dictionary<int, float>();
 		Dictionary<int, float> oldMouthWeights = new Dictionary<int, float>();
@@ -83,12 +86,18 @@ public class LumpyController : MonoBehaviour
 
 		Dictionary<int, float> newEyeWeights = new Dictionary<int, float>();
 		Dictionary<int, float> newMouthWeights = new Dictionary<int, float>();
+
+		Vector3 expressionRotation = new Vector3(270.0f, 0.0f, 0.0f);
+
 		switch (currentExpression)
 		{
 			case Expression.Neutral:
 				break;
 			case Expression.Surprised:
 				newEyeWeights.Add(1, 100);
+				break;
+			case Expression.Pleased:
+				newMouthWeights.Add(1, 50);
 				break;
 			case Expression.Happy:
 				newEyeWeights.Add(2, 100);
@@ -111,6 +120,7 @@ public class LumpyController : MonoBehaviour
 			case Expression.Sad:
 				newEyeWeights.Add(6, 100);
 				newMouthWeights.Add(4, 100);
+				expressionRotation.x += 10.0f;
 				break;
 			case Expression.Unimpressed:
 				newEyeWeights.Add(7, 100);
@@ -119,16 +129,19 @@ public class LumpyController : MonoBehaviour
 			case Expression.Tired:
 				newEyeWeights.Add(8, 100);
 				newMouthWeights.Add(3, 25);
+				expressionRotation.x += 10.0f;
 				break;
 			case Expression.Asleep:
 				newEyeWeights.Add(9, 100);
-				newMouthWeights.Add(3, 100);
+				newMouthWeights.Add(3, 80);
+				expressionRotation.x += 15.0f;
 				break;
 			case Expression.Inquisitive:
 				newEyeWeights.Add(10, 100);
 				break;
 		}
-		while (timeElapsed < duration)
+
+		while (timeElapsed < duration * 2)
         {
 			for (int i = 1; i < eyes.sharedMesh.blendShapeCount; ++i)
 			{
@@ -149,6 +162,12 @@ public class LumpyController : MonoBehaviour
 				}
 				mouth.SetBlendShapeWeight(i, Mathf.Lerp(oldMouthWeights[i], newWeight, timeElapsed / duration));
 			}
+
+			float deltaTime = duration * 100 * (1.0f - Mathf.Exp(-Time.deltaTime));
+			float smoothTime = Mathf.SmoothStep(0.0f, 1.0f, deltaTime);
+			face.transform.localRotation = Quaternion.Slerp(face.transform.localRotation, Quaternion.Euler(expressionRotation), smoothTime);
+			print(expressionRotation);
+			print(face.transform.localEulerAngles);
             
             yield return null;
             timeElapsed += Time.deltaTime;
@@ -235,7 +254,9 @@ public class LumpyController : MonoBehaviour
 		switch (currentExpression)
 		{
 			case Expression.Asleep:
+				return 30;
 			case Expression.Delighted:
+			case Expression.Pleased:
 			case Expression.Happy:
 			case Expression.Sad:
 			case Expression.Unimpressed:
